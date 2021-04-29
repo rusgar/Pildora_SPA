a59033aa5d829e77ef17a2224dd127bb4f7197cf
rusgar <edamamon@gmail.com>
2021-04-29 10:39:20 +0200

    cambios front

--- resources/views/Inicio/onePage.blade.php
+++ resources/views/Inicio/onePage.blade.php
@@ -3,10 +3,21 @@
 
 <body>
       <section>
-            
-            <img src="https://innovations.upsocial.org/sites/default/files/factoriaf5_logo.png"  width="155px"alt="">
+            <img src="https://innovations.upsocial.org/sites/default/files/factoriaf5_logo.png" width="155px" alt="">
       </section>
       <hr>
-      <p><a href="">One Page</a> || <a href="/secondPage">Segunda Pagina</a></p>
+      <p><a href="">One Page</a> || <a href="/secondPage">Segunda Pagina</a> || <a target="_blank"  href="https://github.com/rusgar/Pildora_SPA">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgpfrQuqG7eEi476W9KS7rCtEK2OXZGQzWIv0fgL6Cf4t22UuV_dsVK7sycWk1wUEB2vk&usqp=CAU" alt="" width="25" class="d-inline-block align-text-top">
+            @rusgar(Edu Rus)</a></p>
+            <div class="jumbotron">
+  <h1 class="display-4">Para Angel</h1>
+  <p class="lead">Sigo estirando la pildora para que la presentes tu el Lunes</p>
+  <hr class="my-4">
+  <p>Recuerda...que no se nos olvida</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Lunes</a>
+</div>
+
+      
 </body>
+
 @endsection
